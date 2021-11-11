d14bd981ddcbe4db5df972f72a087da3bb6fd2a2
veriano <veriano@ukr.net>
2021-11-11 18:59:44 +0200

    working on #2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let body = document.querySelector('body');
+let timeId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+    timeId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        startBtn.setAttribute("disabled", "disabled");
+    }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timeId);
+    startBtn.removeAttribute('disabled');
+})
