1e4a97ef5d722a3fc0d7abcf71f1de203f4f618c
veriano <86019033+veriano@users.noreply.github.com>
2021-11-16 23:20:02 +0200

    update 03-promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,13 +4,16 @@ const form = document.querySelector('.form');
 const delay = form.elements.delay;
 const step = form.elements.step;
 const amount = form.elements.amount;
-  
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  let delayValue = Number(delay.value);
+// let stepValue = Number(step.value);
+let amountValue = Number(amount.value); 
  
-  for (let i = 0; i <= Number(amount.value); i += 1) {
-    let delayValue = Number(delay.value) += Number(step.value);
+  for (let i = 1; i <= amountValue; i += 1) {
+    delayValue += Number(step.value);
     setTimeout(() => {
       createPromise(i, delayValue);
     }, delayValue);
@@ -21,16 +24,17 @@ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     if (shouldResolve) {
-      resolve(Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}`));
+      resolve(Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`));
     } else {
-      reject(Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}`));
+      reject(Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`));
     }
   });
 };
-  createPromise(2, 1500)
+createPromise(2, 1500)
     .then(({ position, delay }) => {
       Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
     })
     .catch(({ position, delay }) => {
       Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
+  
