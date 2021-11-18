32c1b6d76b88dba8c0621f094b6676552be8f2fd
veriano <veriano@ukr.net>
2021-11-18 16:18:17 +0200

    update 03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,40 +1,36 @@
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
-const delay = form.elements.delay;
-const step = form.elements.step;
-const amount = form.elements.amount;
-
-let delayValue = Number(delay.value);
-let stepValue = Number(step.value);
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-
-  for (let i = 1; i <= amount.value; i += 1) {
-    delayValue += stepValue;
-    setTimeout(() => {
-      createPromise(i, delayValue)
+  let firstDelay =  Number(e.target.elements.delay.value);
+  const amountValue = e.target.elements.amount.value;
+    for (let i = 1; i <= amountValue; i++) {
+      createPromise(i, firstDelay)
         .then(({ position, delay }) => {
-Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`)
-        })
-        .catch(({ position, delay }) => {
-reject(Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`));
+       Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+     })
+       .catch(({ position, delay }) => {
+         Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+       });
+      firstDelay += Number(e.target.elements.step.value);
+  }
 })
-    }, delayValue);
-  };
-});
 
 function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-  const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        resolve({position, delay});
-      } else {
-        reject({position, delay});
-      }
-    });
-};
+ return new Promise((resolve, reject)=> {
+   setTimeout(() => {
+     const shouldResolve = Math.random() > 0.3;
+     if (shouldResolve) {
+       resolve({position, delay})
+     } else {
+       reject({position, delay})
+     }
+
+   }, delay)
+ })
+}
 createPromise(2, 1500)
     .then(({ position, delay }) => {
       Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
